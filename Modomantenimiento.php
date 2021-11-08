0f4e4f61d3064cf25dcd70cf3c9be42056ffa1b7
ChrisBerganza <89670010+ChrisBerganza@users.noreply.github.com>
2021-11-07 20:45:16 -0600

    Create Modomantenimiento.php

---
+++ Modomantenimiento.php
@@ -0,0 +1,12 @@
+<? php
+
+espacio de nombres  App \ Http \ Middleware ;
+
+use  Illuminate \ Foundation \ Http \ Middleware \ Modomantenimiento  como  Middleware ;
+
+clase  Modomantenimiento  extiende  Middleware
+{
+    
+    protegido  $ excepto = [
+    ];
+}
