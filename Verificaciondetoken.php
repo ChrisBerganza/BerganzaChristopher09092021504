71314b2b3444c35117b49d4ebf30ec608e470a06
ChrisBerganza <89670010+ChrisBerganza@users.noreply.github.com>
2021-11-07 20:57:23 -0600

    Create Verificaciondetoken.php

---
+++ Verificaciondetoken.php
@@ -0,0 +1,12 @@
+<? php
+
+espacio de nombres  App \ Http \ Middleware ;
+
+use  Illuminate \ Foundation \ Http \ Middleware \ Verificaciondetoken  como  Middleware ;
+
+clase Verificaciondetoken  extiende  Middleware
+{
+    protegido  $ excepto = [
+        //
+    ];
+}
